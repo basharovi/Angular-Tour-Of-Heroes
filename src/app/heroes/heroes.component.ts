3aa43cbc5bdd5f342d69870ee200d25dc3923df4
Bashar Ovi <bashar.ovi@outlook.com>
2021-07-10 22:44:38 +0600

    used ngif & click functionality

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,12 +9,18 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = { 
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = { 
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
 
   heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(this.selectedHero?.id);
+  }
 
   constructor() { }
 
